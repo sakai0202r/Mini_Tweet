36499d2e79bd3f5ca4ea06d8fef7362ccd06db52
Renta Sakai <ren_ap2@icloud.com>
2019-08-10 16:39:47 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:notice] = "#{@user.name}さん。Mini Tweetにようこそ！"
       redirect_to @user # = user_url(@user)
     else
