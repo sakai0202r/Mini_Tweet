aaa63d1922bfef46261edfd5d9ee430c4c9f5463
Renta Sakai <ren_ap2@icloud.com>
2019-08-10 15:44:11 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#home'
-  get '/home',  to: 'home#home'
-  get '/about', to: 'home#about'
-  get  '/signup',  to: 'users#new'
+  get '/home',     to: 'home#home'
+  get '/about',    to: 'home#about'
+  get '/signup',   to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
