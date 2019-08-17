b21ccb420ffaf2ec8d3a4ca2b40cbda3dd905d3e
Renta Sakai <ren_ap2@icloud.com>
2019-08-17 14:39:41 +0900

    小さな改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "ログインして下さい"
+      flash[:notice] = "ログインして下さい"
       redirect_to login_url
     end
   end
