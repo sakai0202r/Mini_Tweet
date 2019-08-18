2f8f942c3081528cd1a81c4d027e4dbf14f1b13e
Renta Sakai <ren_ap2@icloud.com>
2019-08-18 17:21:20 +0900

    s

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIASVUJIPC7WQXOG53C',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-
-    config.fog_directory  = 'private.application.sakai.com'
-    config.cache_storage = :fog
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
