a2d86f4498cd1e524dc0da6e5f2fbae92d53a27f
Renta Sakai <ren_ap2@icloud.com>
2019-08-21 15:58:29 +0900

    Lang ja

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MiniTweetApp
   class Application < Rails::Application
     config.load_defaults 5.2
-
+    config.i18n.default_locale = :ja
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
